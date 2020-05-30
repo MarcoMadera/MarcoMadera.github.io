849f1d39d17b54983ca498772a8f0a1c2950f6e1
Marco Madera <marcoseguro2@gmail.com>
2020-05-30 16:52:19 -0700

    [added] responsive hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,75 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles/Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function Navbar() {
+function Navbar(props) {
+  const ref = useRef(null);
+  function handleClick() {
+    ref.current.classList.toggle("show");
+  }
+
+  let location = useLocation();
+  function HeaderView() {
+    let header = "";
+    switch (location.pathname.split("/")[1]) {
+      case "blog":
+        header = "Blog";
+        break;
+      case "portfolio":
+        header = "Portafolio";
+        break;
+      case "about":
+        header = "Sobre mí";
+        break;
+      default:
+        header = "Inicio";
+        break;
+    }
+    return <h3>{header}</h3>;
+  }
+
   return (
     <div className="Navbar" id="myNavbar">
       <div className="container-fluid">
-        <Link className="Navbar__brand" to="/">
-          <span>
-            <span className="font-weight-ligh">Inicio</span>
-          </span>
-        </Link>
-        <Link className="Navbar__brand" to="/blog">
-          <span>
-            <span className="font-weight-ligh">Blog</span>
-          </span>
-        </Link>
-        <Link className="Navbar__brand" to="/portfolio">
-          <span>
-            <span className="font-weight-ligh">Portafolio</span>
-          </span>
-        </Link>
-        <Link className="Navbar__brand" to="/about">
-          <span>
-            <span className="font-weight-ligh">Sobre mí</span>
-          </span>
-        </Link>
+        <div id="sidebar" ref={ref} onClick={handleClick}>
+          <div className="toggleBtn">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul>
+            <Link className="Navbar__brand" to="/">
+              <li>
+                <span>
+                  <span className="font-weight-ligh">Inicio</span>
+                </span>
+              </li>
+            </Link>
+            <Link className="Navbar__brand" to="/blog">
+              <li>
+                <span>
+                  <span className="font-weight-ligh">Blog</span>
+                </span>
+              </li>
+            </Link>
+            <Link className="Navbar__brand" to="/portfolio">
+              <li>
+                <span>
+                  <span className="font-weight-ligh">Portafolio</span>
+                </span>
+              </li>
+            </Link>
+            <Link className="Navbar__brand" to="/about">
+              <li>
+                <span>
+                  <span className="font-weight-ligh">Sobre mí</span>
+                </span>
+              </li>
+            </Link>
+          </ul>
+        </div>
       </div>
+      <div className="Navbar__header">{HeaderView()}</div>
     </div>
   );
 }
